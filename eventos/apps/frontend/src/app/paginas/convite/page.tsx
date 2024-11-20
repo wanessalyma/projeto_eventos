b97763653725e58429a969724d1b904619594ffa
wanessalyma <wanessa.upe@gmail.com>
2024-11-20 15:14:25 -0300

    arquivos de pasta apps

---
+++ eventos/apps/frontend/src/app/paginas/convite/page.tsx
@@ -0,0 +1,7 @@
+export default function PaginaConvite() {
+    return (
+      <div>
+        <span>Convite</span>
+      </div>
+    );
+  }
--- eventos/apps/frontend/src/components/template/Pagina.tsx
+++ eventos/apps/frontend/src/components/template/Pagina.tsx
@@ -1,4 +1,4 @@
-import Logo from "./logo";
+import Logo from "./Logo";
 export interface PaginaProps{
     children: React.ReactNode;
     className?: string;
